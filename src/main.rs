6833e0c52889e5b3029356d74248b8038b29bac4
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-12-15 00:03:14 +0800

    Cached serialized data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 extern crate jemallocator;
 use actix_web::{
-    web, App, Error as ActixErr, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError,
+    web, App, HttpResponse, HttpServer, ResponseError,
 };
 use futures::future::{ok as fut_ok, Either};
 use lru_time_cache::LruCache;
-use lta::bus::bus_arrival::ArrivalBusService;
 use lta::r#async::{bus::get_arrival, lta_client::LTAClient, prelude::*};
 use parking_lot::RwLock;
-use serde::Serialize;
 use std::fmt::Formatter;
 use std::{env::var, time::Duration};
 
@@ -30,79 +28,41 @@ impl ResponseError for JustBusError {
         HttpResponse::InternalServerError().finish()
     }
 }
-#[derive(Serialize, Clone)]
-#[serde(rename_all(serialize = "PascalCase"))]
-struct TimingResult {
-    pub bus_stop_code: u32,
-    pub data: Vec<ArrivalBusService>,
-}
-
-impl TimingResult {
-    pub fn new(bus_stop_code: u32, data: Vec<ArrivalBusService>) -> Self {
-        TimingResult {
-            bus_stop_code,
-            data,
-        }
-    }
-}
-
-impl Responder for TimingResult {
-    type Error = ActixErr;
-    type Future = Result<HttpResponse, ActixErr>;
-
-    fn respond_to(self, _: &HttpRequest) -> Self::Future {
-        let body = serde_json::to_string(&self.data)?;
-
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(body))
-    }
-}
 
 fn get_timings(
     path: web::Path<u32>,
     lru: web::Data<RwLock<LruCacheU32>>,
     client: web::Data<LTAClient>,
-) -> impl Future<Item = HttpResponse, Error = JustBusError> {
+) -> impl Future<Item = HttpResponse, Error = JustBusError>{
     let inner_path = path.into_inner();
     let lru_state_2 = lru.clone();
     let inner = lru.read();
     let in_lru = inner.peek(&inner_path);
 
     match in_lru {
-        Some(data) => Either::A(fut_ok(
-            HttpResponse::Ok().json(TimingResult::new(inner_path, data.clone().data)),
-        )),
-        None => {
-            println!(
-                "Fresh data from LTA. client_ptr: {:p}, cache_ptr: {:p}",
-                &client, &lru
-            );
-            Either::B(
-                get_arrival(&client, inner_path, None)
-                    .then(move |r| {
-                        r.map(|r| {
-                            let bus_stop = inner_path;
-                            let data = r.services.clone();
-                            let mut lru_w = lru_state_2.write();
-                            lru_w.insert(bus_stop, TimingResult::new(bus_stop, data));
-                            r
-                        })
+        Some(f) => Either::A(fut_ok(HttpResponse::Ok().content_type("application/json").body(f))),
+        None => Either::B(
+            get_arrival(&client, inner_path, None)
+                .then(move |r| {
+                    r.map(|r| {
+                        let bus_stop = inner_path;
+                        let data = serde_json::to_string(&r.services).unwrap();
+                        let mut lru_w = lru_state_2.write();
+                        lru_w.insert(bus_stop, data.clone());
+                        data
                     })
-                    .map(|f| {
-                        HttpResponse::Ok().json(TimingResult::new(f.bus_stop_code, f.services))
-                    })
-                    .map_err(JustBusError::ClientError),
-            )
-        }
+                })
+                .map(|f| HttpResponse::Ok().content_type("application/json").body(f))
+                .map_err(JustBusError::ClientError),
+        ),
     }
 }
 
-type LruCacheU32 = LruCache<u32, TimingResult>;
+type LruCacheU32<'a> = LruCache<u32, String>;
 
 fn main() {
     println!("Starting server @ 127.0.0.1:8080");
-    let api_key = var("API_KEY").unwrap();
+    let api_key = var("API_KEY").expect("API_KEY missing!");
     let ttl = Duration::from_millis(1000 * 15);
     let client = web::Data::new(LTAClient::with_api_key(api_key));
     let lru_cache = web::Data::new(RwLock::new(LruCacheU32::with_expiry_duration(ttl)));
