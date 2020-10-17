55517f36afc4a299c05c7782485fbfecbc680ee5
BudiNverse <me@inve.rs>
2020-10-18 04:30:13 +0800

    Changed tls key file import name

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ const SZ: usize = 500;
 fn load_ssl_keys() -> ServerConfig {
     let mut config = ServerConfig::new(NoClientAuth::new());
     let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
-    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("key-rsa.pem").unwrap());
     let cert_chain = certs(cert_file).unwrap();
     let mut keys = rsa_private_keys(key_file).unwrap();
     config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
