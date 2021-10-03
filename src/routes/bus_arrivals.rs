4ed72f86a1c5e71eec294cb0bd0e06adf1b5a322
Budi Syahiddin <me@inve.rs>
2021-10-03 19:03:54 +0800

    Update route impl

--- src/routes/bus_arrivals.rs
+++ src/routes/bus_arrivals.rs
@@ -1,6 +1,3 @@
-#[cfg(feature = "cht")]
-use cht_time::Cache;
-
 #[cfg(feature = "dashmap")]
 use dashmap_time::Cache;
 
@@ -19,24 +16,17 @@ use log::info;
 use lta::Bus;
 use lta::{BusRequests, LTAClient};
 
-#[cfg(any(feature = "cht", feature = "dashmap"))]
+#[cfg(feature = "dashmap")]
 pub async fn bus_arrivals(
     bus_stop: web::Path<u32>,
     lru: web::Data<Cache<u32, String>>,
     client: web::Data<LTAClient>,
 ) -> JustBusResult {
     let bus_stop = bus_stop.into_inner();
-
-    #[cfg(any(feature = "cht", feature = "dashmap"))]
     let in_lru = lru.get(&bus_stop);
 
     let res = match in_lru {
-        #[rustfmt::skip]
-        Some(f) => {
-            #[cfg(feature = "dashmap")] let response = HttpResponse::Ok().content_type("application/json").body(&f.value);
-            #[cfg(feature = "cht")] let response = HttpResponse::Ok().content_type("application/json").body(f);
-            response
-        }
+        Some(f) => HttpResponse::Ok().content_type("application/json").body(&f.value),
         None => {
             #[cfg(feature = "logging")]
             info!(
@@ -85,7 +75,6 @@ pub async fn bus_arrivals(
 
             let mut lru_w = lru.write();
             let arrival_str = serde_json::to_string(&arrivals.services).unwrap();
-
             lru_w.insert(bus_stop, arrival_str.clone());
 
             HttpResponse::Ok()
